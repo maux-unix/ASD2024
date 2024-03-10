a1db9c620c7e8f0293e21e7f979071b4e1a71688
Maulana Ali <maulanalazim5@gmail.com>
2024-03-10 22:54:15 +0700

    Messing Around by Using STL C++

---
+++ 1_ll/single/single2.cpp
@@ -0,0 +1,31 @@
+/*
+    SINGLE LINKED LIST
+    MAULANA ALI
+*/
+
+#include <bits/stdc++.h>
+
+typedef struct Member {
+    int num;
+    std::string name;
+} m;
+
+void print(const std::forward_list<m>& members) {
+    std::cout << "Members = {";
+    for (const Member& member : members) {
+        std::cout << '(' << member.num << ", " << member.name << ") -> ";
+    }
+    std::cout << "}";
+}
+
+int main() {
+    Member m1 = {1, "One"};
+    Member m2= {2, "Two"};
+    std::forward_list<m> members;
+
+    members.push_front(m1);
+    members.push_front(m2);
+    print(members);
+    
+    return 0;
+}
